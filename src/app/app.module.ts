2f64588a2b7529f56be51a7dd4e2ea1e90c30045
Keith Stewart <kgstewart@gmail.com>
2016-11-14 16:29:38 -0600

    [#1]: Lazy Loading the Map
    * hoot up the router with a /home and /map route
    * set the header to be visible in both routes with a navigation menu
    * create a map property on the esri-map component so that the map
    * 	configuration is cached when navigating around the app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
+import { MaterialModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
 import { EsriMapComponent } from './esri-map/esri-map.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    EsriMapComponent
+    EsriMapComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    MaterialModule.forRoot(),
+    RouterModule.forRoot([
+      { path: 'home', component: HomeComponent },
+      { path: 'map', component: EsriMapComponent },
+      { path: '**', redirectTo: '/home' }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
