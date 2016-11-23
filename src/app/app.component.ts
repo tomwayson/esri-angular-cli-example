8e5999abbe578aedaceed1e3aba7e70358c01cc1
Tom Wayson <tom@tomwayson.com>
2016-11-22 21:40:14 -0800

    fix esri-loader import and update repo name in title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Angular CLI Esri';
+  title = 'Esri Angular CLI Example';
 }
